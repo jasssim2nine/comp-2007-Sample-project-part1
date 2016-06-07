d18d8ca47fd6487a036ef40f0fc60005c9522f56
Jasssim <Jasssim@DESKTOP-KNOLH7M>
2016-06-07 12:26:03 -0400

    Add project files.

---
+++ comp-2007-Sample-project-part1/Default.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Frontend.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="comp_2007_Sample_project_part1.Default" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <bs29:Jumbotron ID="Jumbotron" runat="server"/>
+</asp:Content>
---
+++ comp-2007-Sample-project-part1/User_Control/Navbar.ascx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace comp_2007_Sample_project_part1.User_Control
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+        /**
+        * This method adds a css class of "active" to list items related
+        * to navigation links of each page 
+        * 
+        * @method SetActivePage
+        * @return {void}
+        */
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
